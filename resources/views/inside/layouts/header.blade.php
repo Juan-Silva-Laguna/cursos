10e6c773558ef63448cad97400525174efa2bbda
Davian Ascanio <davianascanio1997@gmail.com>
2022-11-30 22:47:02 -0500

    fix: styles

--- resources/views/inside/layouts/header.blade.php
+++ resources/views/inside/layouts/header.blade.php
@@ -1,5 +1,5 @@
 
-<div class="grid bg-dark">
+<div class="grid bg-dark" style="border-bottom: 1px solid #3f3f3f ">
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark container">
   <a class="navbar-brand" href="#"> <b> ELITE <span style="color: #FFB801">ACADEMY</span>  </b></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -8,16 +8,16 @@
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Curso <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="/home">Cursos <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Clientes por Activar</a>
+        <a class="nav-link" href="/usuarios/0">Clientes por Activar</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Mis Clientes Activos</a>
+        <a class="nav-link" href="/usuarios/1">Mis Clientes Activos</a>
       </li>
        <li class="nav-item">
-        <a class="nav-link" href="#">Testimonios</a>
+        <a class="nav-link" href="/testimonios">Testimonios</a>
       </li>
       <form id="logout-form" action="{{ route('logout') }}" method="POST">
         @csrf
--- resources/views/inside/testimonios.blade.php
+++ resources/views/inside/testimonios.blade.php
@@ -1,30 +1,47 @@
-@extends('outside.layouts.body')
+@extends('inside.layouts.body')
+@section('dashboard')
 
-@section('content')
-    <div class="slide"></div>
+<div class="container  bg-dark">
+    <div class="row">
+    <div class="col-6">    
+        <main class="main" style="overflow: hidden">
+        <img class="background-item-1" style="position: absolute; height: 100% !important;" src="{{Vite::asset('resources/images/background-item-1.svg')}}" alt="" />
+        <div class="container" style="position: relative;">
+            <section class="wrapper">
+                <div class="heading">
+                    <h1 class="text text-large">Crear Testimonio</h1>
+                </div>
+                <form class="form" action="{{ route('testimonios_save') }}" method="POST" accept-charset="UTF-8">
+                   <input name="_token" type="hidden" value="{{ csrf_token() }}"/>
+                    <div class="input-control">
+                        <label for="descripcion" class="input-label" hidden>Descripción</label>
+                        <input class="input-field" placeholder="Descripción" id="descripcion" type="text" class="form-control" name="descripcion" required>
+                    </div>
+                    <div class="input-control">
+                        <label for="password" class="input-label" hidden>Satisfación</label>
+                        <input class="input-field" placeholder="Satisfación" id="satisfacion" type="number"  min="1" max="5" class="form-control" name="satisfacion" required>
+                    </div>
+                    <div class="input-control">
+                        <input type="submit" name="submit" class="input-submit" value="Guardar">
+                    </div>
+                </form>
+            </section>
+        </div>
+    </main>
     </div>
-      <div class="previous"></div>
-      <div class="next"></div>
-    </div>
-
-    <h1>{{ $mensaje }}</h1>
-    
-    
-    <form action="{{ route('testimonios_save') }}" method="POST" accept-charset="UTF-8">
-        <input name="_token" type="hidden" value="{{ csrf_token() }}"/>
-        <label for="descripcion">descripcion:</label>
-        <input name="descripcion" type="text" value=""/>
-        <label for="satisfacion">satisfacion:</label>
-        (5 estrellas o select)<input name="satisfacion" type="number" max="5" value=""/>
-        <button type="submit">Send</button>
-    </form>
-
-    <table>
+    <div class="col-6 mt-5">
+         <h1>{{ $mensaje }}</h1>
+        <div class="card-title">
+             <h5 class="m-0"><b> TESTIMONIOS </b></h5>
+        </div>
+         
+        <table class="table mt-4">
             <thead>
                 <tr>
                     <th>Testimonio</th>
                     <th>Usuario</th>
                     <th>Nivel Satisfacion</th>
+                    <th></th>
                 </tr>
             </thead>
             <tbody>
@@ -33,11 +50,16 @@
                     <th>{{$test->descripcion}}</th>
                     <th>{{$test->name}}</th>
                     <th>{{$test->satisfacion}}</th>
-                    <th><a href="https://wa.me/57{{ $test->celular }}">click aquí</a></th>
+                    <th><a target="_blank" href="https://wa.me/57{{ $test->celular }}">Click Aquí</a></th>
                 </tr>
             @endforeach
                 
             </tbody>
         </table>
+           @if(count($testimonios) == 0)
+            <span>No se encontraron registros</span>
+            @endif
+                
+    </div>
 @endsection
 
